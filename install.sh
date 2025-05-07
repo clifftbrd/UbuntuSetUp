d37594ff0a64b7cca08650f19741c7eb676e4b6c
Cliff <cliff_abbio@yahoo.com>
2025-05-06 21:54:19 -0400

    added install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+distro=$(\cat /etc/os-release | grep "^NAME")
+distrolist="Arch Ubuntu Debian"
+
+for distrofinal in $distrolist
+do
+ [[ $distro =~ $distrofinal ]] && distroset=$distrofinal;
+done
+
+echo $distroset
+
+
